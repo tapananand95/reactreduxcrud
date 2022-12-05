e0dd25d0ed22c381fae70c72f8e4129e1f1fd65a
Tapan Anand <70485285+tpndevstech@users.noreply.github.com>
2022-12-05 19:30:34 +0530

    Update contactReducer.js

--- src/redux/reducers/contactReducer.js
+++ src/redux/reducers/contactReducer.js
@@ -1,6 +1,34 @@
 const initialState = [
-  { id: 0, name: "Rajesh Kumar", email: "rajkumar04@email.com", phone: 1234567890 },
-  { id: 1, name: "Test Name", email: "testname@email.com", phone: 4567891230 },
+  {
+    id: 0,
+    name: "Test Name 1",
+    email: "testname01@email.com",
+    phone: 1234567890,
+  },
+  {
+    id: 2,
+    name: "Test Name 2",
+    email: "test2name@email.com",
+    phone: 4567891230,
+  },
+  {
+    id: 3,
+    name: "Test Name 3",
+    email: "testname3@email.com",
+    phone: 5678912340,
+  },
+  {
+    id: 4,
+    name: "Test Name 4",
+    email: "T4estname@email.com",
+    phone: 7890123456,
+  },
+  {
+    id: 5,
+    name: "Test Name 5",
+    email: "Test5.name@email.com",
+    phone: 2345678901,
+  },
 ];
 
 export const contactReducer = (state = initialState, action) => {
